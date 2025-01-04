17693a7ed4aab31a7f0e329c7eb99d18970889e9
Viacheslav <viacheslav.webdesign@gmail.com>
2025-01-04 16:50:00 +0200

    api test

--- helpers/getPageData.ts
+++ helpers/getPageData.ts
@@ -7,7 +7,9 @@ import { useI18n } from "vue-i18n";
 export const getPageData = async (slug: string) => {
   const { locale } = useI18n();
   try {
-    const response = await $fetch(`/api/wp?slug=${slug}&lang=${locale.value}`);
+    const response = await $fetch(
+      `https://thatspecial.dev/api/wp?slug=${slug}&lang=${locale.value}`
+    );
     return response;
   } catch (error) {
     console.error("API call error:", error);
